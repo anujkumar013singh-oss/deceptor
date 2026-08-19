7c48f92fc960db9e0e073c1a602ac25a50821587
ANUJ SINGH <anuj@ANUJs-MacBook-Pro.local>
2026-08-19 16:05:31 +0530

    fix(vercel): declare type commonjs in root package.json for lambda commonjs module resolution

--- api/health.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({
-    status: 'ok',
-    message: 'Deceptor API Serverless Engine is ONLINE',
-    time: new Date().toISOString(),
-  });
-};
